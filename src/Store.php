e57a33dec0d24c0d104a0f0265174d7e67d84281
Ashlin Aronin <ashlinaronin@gmail.com>
2015-08-28 09:46:41 -0700

    Make getName pass in Store

--- src/Store.php
+++ src/Store.php
@@ -9,44 +9,47 @@
 
         function __construct($name, $location, $phone, $id = null)
         {
-
+            $this->name = $name;
+            $this->location = $location;
+            $this->phone = $phone;
+            $this->id = $id;
         }
 
 
         // Getters and Setters
         function setName($new_name)
         {
-
+            $this->name = $new_name;
         }
 
         function getName()
         {
-
+            return $this->name;
         }
 
         function setLocation($new_location)
         {
-
+            $this->location = $new_location;
         }
 
         function getLocation()
         {
-
+            return $this->location;
         }
 
         function setPhone($new_phone)
         {
-
+            $this->phone = $new_phone;
         }
 
         function getPhone()
         {
-
+            return $this->phone;
         }
 
         function getId()
         {
-
+            return $this->id;
         }
 
 
