389db0f9898b2472a2228650a193a59f503e79f2
Ashlin Aronin <ashlinaronin@gmail.com>
2015-08-28 10:18:56 -0700

    Pass deleteAll test in Brand

--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -68,29 +68,29 @@
             //Assert
             $this->assertEquals([$test_brand, $test_brand2], $result);
         }
-        //
-        // function test_deleteAll()
-        // {
-        //     //Arrange
-        //     $name = "Burchs";
-        //     $location = "Oakway Center";
-        //     $phone = "5415131122";
-        //     $test_store = new Store($name, $location, $phone);
-        //     $test_store->save();
-        //
-        //     $name2 = "Payless ShoeSource";
-        //     $location2 = "Valley River Center";
-        //     $phone2 = "5415130809";
-        //     $test_store2 = new Store($name2, $location2, $phone2);
-        //     $test_store2->save();
-        //
-        //     //Act
-        //     Store::deleteAll();
-        //
-        //     //Assert
-        //     $result = Store::getAll();
-        //     $this->assertEquals([], $result);
-        // }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $name = "Nike";
+            $website = "http://www.nike.com";
+            $test_brand = new Brand($name, $website);
+            $test_brand->save();
+
+            $name2 = "Adidas";
+            $website2 = "http://www.adidas.com";
+            $test_brand2 = new Brand($name2, $website2);
+            $test_brand2->save();
+
+            //Act
+            Brand::deleteAll();
+
+            //Assert
+            $result = Brand::getAll();
+            $this->assertEquals([], $result);
+        }
+
+
         //
         // function test_find()
         // {
